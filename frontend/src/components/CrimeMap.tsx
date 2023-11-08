b3aac3769c460d0951ce94228029e3fc1f5e44f7
Epyonn <pdavisnguyen1@outlook.com>
2023-11-08 15:49:50 -0800

    Changed post api endpoint to Heroku

--- frontend/src/components/CrimeMap.tsx
+++ frontend/src/components/CrimeMap.tsx
@@ -64,9 +64,11 @@ const CrimeMap: React.FC = () => {
         const fetchLocations = async () => {
         try {
             // Make a GET request to the server to retrieve the collection.
+
             //const response = await axios.get('http://localhost:5038/todoappcollection');
 
             const response = await axios.get('https://obscure-scrubland-34656-8bc24b932b2d.herokuapp.com/todoappcollection')
+            //const response = await axios.get('http://localhost:5038/todoappcollection');
             // Extract the location strings from the response data.
             const locationStrings = response.data.map((item: any) => item.location);
 
